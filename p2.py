210a471fc5c9db3e3fc0826764e060374f9591cb
bimbimprasetyoafif <bimo.prasetyoafif@gmail.com>
2020-03-06 13:44:34 +0700

    Init Project

---
+++ p2.py
@@ -0,0 +1,34 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontface_default.xml')
+
+img = cv2.imread('wajah6.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+c = 2
+l = len(faces)+1
+fig = plt.figure()
+
+fig.add_subplot(l,2,2)
+plt.imshow(img)
+
+for (x,y,w,h) in faces:
+    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+    roi_color = img[y:y+h, x:x+w]
+    
+    c = c + 1
+    fig.add_subplot(l,2,c)
+    plt.imshow(roi_color)
+
+    c = c + 1
+    fig.add_subplot(l,2,c)
+    plt.hist(roi_color.ravel(),256,[0,256])
+
+plt.show()
+
